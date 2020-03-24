df648b45f82a794a16abf48927fa3692c2487243
Officer-Pringle <54556056+Officer-Pringle@users.noreply.github.com>
2020-03-24 01:47:57 -0400

    some shit idk

--- lua/autorun/photon/library/auto/14_impala_Components.lua
+++ lua/autorun/photon/library/auto/14_impala_Components.lua
@@ -11,7 +11,7 @@ local G = "GREEN"
 local RB = "BLUE/RED"
 
 
-local name = "P 2014 Impala Wig Wags"
+local name = "P 2014 Impala Taillights"
 COMPONENT = {}
 
 COMPONENT.Skin = 0
@@ -37,7 +37,7 @@ COMPONENT.Meta = {
 	tail_right = {
 		AngleOffset = -90,
 		W = 9.8,
-		H = 8.5,
+		H = 8.6,
 		Sprite = "sprites/emv/impala_tail_right",
 		Scale = 1,
 		WMult = 2,
@@ -49,14 +49,14 @@ COMPONENT.Meta = {
 COMPONENT.Positions = {
 
 	[1] = { 
-		Vector( -30, -112.5, 46.2), 
+		Vector( -30.6, -114.85, 50.6), 
 		Angle( 0, 168, 20), 
-		"tail_left" 
+		"tail_left", 5, 10, "RED"
 	},
 	[2] = { 
-		Vector( 30, -113, 46.2 ), 
-		Angle( 0, 168, 20 ), 
-		"tail_right"  
+		Vector( 30.6, -114.85, 50.6), 
+		Angle( 1, 195, 20 ), 
+		"tail_right", 10 , 10, "RED"  
 	},
 
 }
@@ -81,6 +81,84 @@ COMPONENT.Modes = {
 		M1 = { ["auto_rear_taillights"] = "code1", },
 		M2 = { ["auto_rear_taillights"] = "code2", },
 		M3 = { ["auto_rear_taillights"] = "code3", }
+	},
+	Auxiliary = {},
+	Illumination = {}
+}
+
+EMVU:AddAutoComponent( COMPONENT, name )
+
+local name = "14 Impala Headlight Flashers"
+COMPONENT = {}
+
+COMPONENT.Skin = 0
+COMPONENT.Bodygroups = {}
+COMPONENT.NotLegacy = true
+COMPONENT.ColorInput = 1
+COMPONENT.UsePhases = true
+COMPONENT.Category = "Hidden"
+COMPONENT.DefaultColors = {
+	[1] = "WHITE",
+	[2] = "WHITE",
+}
+
+COMPONENT.Meta = {
+	front_left = {
+		AngleOffset = -90,
+		W = 5,
+		H = 5,
+		Sprite = "sprites/emv/light_circle",
+		Scale = .5,
+		WMult = 1,
+	},
+	front_right = {
+		AngleOffset = -90,
+		W = 5,
+		H = 5,
+		Sprite = "sprites/emv/light_circle",
+		Scale = .5,
+		WMult = 1,
+		
+	},
+	
+}
+
+COMPONENT.Positions = {
+
+	[1] = { 
+		Vector( -34, 101., 37), 
+		Angle( -7, 0, 0), 
+		"front_left", 5
+	},
+	[2] = { 
+		Vector( 34, 101., 37), 
+		Angle( -7, 0, 0),
+		"front_right", 5
+	},
+
+}
+
+COMPONENT.Sections = {
+	["auto_headlights"] = {
+		[1] = { { 1, "_1" }, },
+		[2] = { { 2, "_2" },  },
+	},
+}
+
+COMPONENT.Patterns = {
+	["auto_headlights"] = {
+		["code1"] = { 2,2,2,2,2,2,0,1,1,1,1,1,1},
+		["code2"] ={ 2,2,2,0,2,2,2,0,1,1,1,0,1,1,1,0},
+		["code3"] = { 2,0,2,0,1,0,1 }
+		
+	}
+}
+
+COMPONENT.Modes = {
+	Primary = {
+		M1 = { ["auto_headlights"] = "code1", },
+		M2 = { ["auto_headlights"] = "code2", },
+		M3 = { ["auto_headlights"] = "code3", }
 	},
 	Auxiliary = {},
 	Illumination = {}
